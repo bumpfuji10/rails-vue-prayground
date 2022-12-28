0fc0347a1e2f7f33b084f491feea501790453dad
Yo Kamada <bumpfuji10@gmail.com>
2022-12-28 22:20:37 +0900

    add:eventモデルにバリデーション設定

--- db/migrate/20221228124235_create_events.rb
+++ db/migrate/20221228124235_create_events.rb
@@ -1,10 +1,11 @@
 class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
-      t.string :name
-      t.datetime :start
-      t.datetime :end
-      t.boolean :timed
+      t.string :name, limit: 100, null: false
+      t.datetime :start, null: false
+      t.datetime :end, null: false
+      t.boolean :timed, default: true
+
       t.text :description
       t.string :color
 
